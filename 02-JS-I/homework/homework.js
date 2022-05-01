ce9fd7d6385f8e8b10202208005c2ce05b7b42c4
gjguerram <gjguerram@gmail.com>
2022-05-01 07:05:22 -0300

    resolviendo ejerciciosss

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -36,31 +36,38 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  
+  return (x + y);
+
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  
+  return (x - y);
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  
+  return (x * y);
+
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  
+  return (x / y);
+
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (x == y)
+  return true
+  else 
+  return false; 
   
 }
 
@@ -68,14 +75,28 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  var str1 = String
+  var str2 = String
+  if (str1 == str2)
+  return true
+  else
+  return false;
+
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  function menosquenoventa(num) {
+    function num(a) {
+        if (a < 90)
+        return true
+        else (a > 90)
+        return false;}
+
+}
+
 }
 
 function mayorQueCincuenta(num) {
