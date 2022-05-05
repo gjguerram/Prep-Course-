875ae35b0e5be13691e6a46b512ec0b2356f870f
gjguerram <gjguerram@gmail.com>
2022-05-05 12:14:57 -0300

    resolviendo ejercicios

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -251,12 +251,11 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  var cantidad = 
-  euro == 1.20
-  return (euro * cantidad);
-
-
+  var euroadolar = 1.20 
+  return euro * euroadolar
+  
 }
+
 
 
 function esVocal(letra){
